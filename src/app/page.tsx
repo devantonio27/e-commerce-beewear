9e0acc3188767291ba8995224e07dcfe3f57bcdd
Tonidev <antoniodev2709@gmail.com>
2026-01-30 00:33:11 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
@@ -12,6 +15,12 @@ const Home = async () => {
     },
   });
 
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
   const categories = await db.query.categoryTable.findMany({});
 
   return (
@@ -45,6 +54,9 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
