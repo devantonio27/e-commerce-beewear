7e272ecaa4b11b2a19eb381ffe4a0809b0bf7302
Tonidev <antoniodev2709@gmail.com>
2026-02-10 22:18:20 -0300

    feat: ensure product quantity can be decreased

--- src/actions/decrease-cart-product-quantity/index.ts
+++ src/actions/decrease-cart-product-quantity/index.ts
@@ -8,12 +8,12 @@ import { db } from "@/db";
 import { cartItemTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
-import { removeProductFromCartSchema } from "./schema";
+import { decreaseCartProductQuantityschema } from "./schema";
 
-export const removeProductFromCart = async (
-  data: z.infer<typeof removeProductFromCartSchema>,
+export const decreaseCartProductQuantity = async (
+  data: z.infer<typeof decreaseCartProductQuantityschema>,
 ) => {
-  removeProductFromCartSchema.parse(data);
+  decreaseCartProductQuantityschema.parse(data);
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -33,6 +33,13 @@ export const removeProductFromCart = async (
   if (cartDoesNotBelongToUser) {
     throw new Error("Unauthorized");
   }
+  if (cartItem.quantity === 1) {
+    await db.delete(cartItemTable).where(eq(cartItemTable.id, cartItem.id));
+    return;
+  }
 
-  await db.delete(cartItemTable).where(eq(cartItemTable.id, cartItem.id));
+  await db
+    .update(cartItemTable)
+    .set({ quantity: cartItem.quantity - 1 })
+    .where(eq(cartItemTable.id, cartItem.id));
 };
